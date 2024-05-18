f5589aaf8f7d7a83d5e7ff9cfcb4d32c62fd9cd1
Piyush Chaudhari <piyushchaudhari112@gmail.com>
2024-05-18 19:48:17 +0530

    Added ne line

---
+++ index.js
@@ -0,0 +1,9 @@
+const dict={
+    appreciate: "recognize the full worth of",
+    ataraxia: "a state of freedom from emotional disturbance and anxiety",
+    yakka: "work, especially hard work"
+  }
+  
+  console.log(dict['yakka'])
+
+  
